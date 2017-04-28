8bb8efe10d8edb1574e7e57f55456af88c526b5e
Jessica Andzouana <jandzouana@gmail.com>
2017-04-28 13:21:52 -0700

    added scene elements

---
+++ ArtGallery/Assets/OvrAvatar/Scripts/OvrAvatarHand.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+
+public class OvrAvatarHand : MonoBehaviour, IAvatarPart
+{
+
+    float alpha = 1.0f;
+
+    public void UpdatePose(OvrAvatarDriver.ControllerPose pose)
+    {
+	//This will enable collisions only when the hand trigger is pressed enough to make a fist.
+		 /*
+		 if (GetComponent<Rigidbody>()  != null)
+		{
+			GetComponent<Rigidbody>().detectCollisions = pose.handTrigger >= 0.75f;
+		}
+		*/
+    }
+
+    public void SetAlpha(float alpha)
+    {
+        this.alpha = alpha;
+    }
+
+    public void OnAssetsLoaded()
+    {
+        SetAlpha(this.alpha);
+    }
+}
---
+++ ArtGallery/Assets/Scripts/AvatarCalibrator.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AvatarCalibrator : MonoBehaviour
+{
+
+    public GameObject LeftHand;
+    public GameObject LeftHandAvatar;
+
+    // Use this for initialization
+    void Start()
+    {
+        if (LeftHand == null)
+            LeftHand = GameObject.Find("LeftHandAnchor");
+            if (LeftHandAvatar == null)
+                LeftHandAvatar = transform.FindChild("hand_left").gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += LeftHand.transform.position - LeftHandAvatar.transform.position;
+    }
+}
